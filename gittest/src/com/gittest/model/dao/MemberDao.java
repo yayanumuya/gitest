e4918f54406337b7571af0ddaeb61ee716e23f3d
goodee <goodee@DESKTOP-5IGTVNI>
2023-10-18 11:30:21 +0900

    bs_이름조회기능 구현중..; selectMemberName메소드 추가

--- gittest/src/com/gittest/model/dao/MemberDao.java
+++ gittest/src/com/gittest/model/dao/MemberDao.java
@@ -1,7 +1,14 @@
 package com.gittest.model.dao;
 
+import java.sql.Connection;
+import java.util.List;
 import java.util.Properties;
 
 public class MemberDao {
 	private Properties sql=new Properties();
+	
+	public List<String> selectMemberName(Connection conn){
+		
+		return null;
+	}
 }
