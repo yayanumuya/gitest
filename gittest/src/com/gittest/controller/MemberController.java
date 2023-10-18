b0a0f9df3c18671b4e39f8cd94f5334be03f7973
goodee <goodee@DESKTOP-5IGTVNI>
2023-10-18 10:34:50 +0900

    bs_controller에 메소드만추가

--- gittest/src/com/gittest/controller/MemberController.java
+++ gittest/src/com/gittest/controller/MemberController.java
@@ -5,4 +5,10 @@ public class MemberController {
 	public void selectMemberAll() {
 		System.out.println("개발중...");
 	}
+	public void selectMemberById() {
+		System.out.println("개발중");
+	}
+	public void inserMember() {
+		
+	}
 }
